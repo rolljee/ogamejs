8ad1c096baf44a34f46605af4fc58c8421e88a2c
Rolljee <n.riquelmebareiro@gmail.com>
2020-04-10 18:52:17 +0200

    remove winston as a deps

--- src/winston.js
+++
@@ -1,9 +0,0 @@
-import winston from 'winston';
-
-export default winston.createLogger({
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-  ],
-});
