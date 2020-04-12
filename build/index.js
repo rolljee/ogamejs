5b3dfd036d3a0c7ca46f91afd7dd986bcb41dc88
Rolljee <n.riquelmebareiro@gmail.com>
2020-04-12 16:57:27 +0200

    gitignore

--- build/index.js
+++ build/index.js
@@ -11,12 +11,20 @@ var _buildings = _interopRequireDefault(require("./buildings"));
 
 var _fleets = _interopRequireDefault(require("./fleets"));
 
+var _buildings2 = _interopRequireDefault(require("./models/buildings"));
+
+var _destroyable = _interopRequireDefault(require("./models/destroyable"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var Ogame = {
   Trader: _trades["default"],
   Building: _buildings["default"],
-  Fleets: _fleets["default"]
+  Fleets: _fleets["default"],
+  models: {
+    Buildings: _buildings2["default"],
+    Destroyable: _destroyable["default"]
+  }
 };
 var _default = Ogame;
 exports["default"] = _default;
