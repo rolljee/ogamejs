b2e145976e31bc912fb5654627f1f479b9490779
Rolljee <n.riquelmebareiro@gmail.com>
2020-04-12 16:29:50 +0200

    Export models

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import Trader from './trades';
 import Building from './buildings';
 import Fleets from './fleets';
+import Buildings from './models/buildings';
+import Destroyable from './models/destroyable';
 
 const Ogame = {
   Trader,
   Building,
   Fleets,
+  models: {
+    Buildings,
+    Destroyable,
+  },
 };
 
 export default Ogame;
